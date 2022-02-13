9ec9b091f1bdea43e96b675c4aef06ff3878db1d
setton823 <97462865+setton823@users.noreply.github.com>
2022-02-13 22:21:21 +0900

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,11 @@
+number = 1
+number2 = 1.0
+greeting = 'こんにちは'
+is_ok = True
+
+print(number, type(number))     #typeは型を表示する
+print(number2, type(number2))
+print(greeting, type(greeting))
+print(is_ok, type(is_ok))
+print(2 > 1)
+print(2 < 1)
