8a4b1bdd1001940ab7c671ead5dc78e136649e45
setton823 <97462865+setton823@users.noreply.github.com>
2022-02-14 06:57:00 +0900

    Add files via upload

---
+++ string.py
@@ -0,0 +1,19 @@
+print("Hello World!")
+print("Hello, \nWorld!")
+print("""こんにちは、
+おはようございます、
+こんばんわ""")
+print("もう雪は" + "降らないで")
+print("雪" * 5)     #文字列の繰り返し
+print("雪" * 5 + "まみれ")
+print(1 + 1)
+print("1" + "1")
+print("1 + 1")
+print("Mika")
+print("Mika"[0])   #[]内の数字番目（ここでは0番目)の値を取り出す
+print("12345"[0])
+print("Mika"[-1])   #末尾から取り出す
+print("English"[1:5])   #後ろの数字は含まない
+print("English"[2:])
+print("English"[:4])
+print("English"[4])
