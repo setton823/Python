5886fb4075abfe7d24d1557e2d02caade45000a6
setton823 <97462865+setton823@users.noreply.github.com>
2022-03-01 15:54:03 +0900

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,25 @@
+numbers = (1, 2, 3, 4)
+numbers 
+(1, 2, 3, 4)       #tuple型は値を変更できない
+type(numbers)
+<class 'tuple'>
+numbers[0]
+1
+numbers[3]
+4
+a = (1, 2, 3)
+b = (4, 5, 6)
+a
+(1, 2, 3)
+b
+(4, 5, 6)
+c = (a, b)
+c
+((1, 2, 3), (4, 5, 6))
+a + b
+(1, 2, 3, 4, 5, 6)
+numbers = 1, 2, 3, 4,
+numbers
+(1, 2, 3, 4)
+type(numbers)
+<class 'tuple'>
